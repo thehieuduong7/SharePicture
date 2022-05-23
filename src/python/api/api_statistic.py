fc7855d04eba2b53273abb2655d799f414f36fda
thehieuduong7 <hieuduon959@gmail.com>
2022-05-24 03:29:00 +0700

    statis

--- src/python/api/api_statistic.py
+++ src/python/api/api_statistic.py
@@ -2,16 +2,20 @@ from __init__ import app, my_login
 import json
 from flask_login import login_user
 from models import UserLoginModel
-from src.python.service.Service import AuthService, StatisticService
-
-static = StatisticService()
 
 
-@app.route('/static/shared')
-def statisticShare():
-    list = static.getStatisticShared()
+@app.route('/static')
+def statisticApi():
+    all = UserLoginModel.query.all()
     res = []
-    for i in list:
-        u = UserLoginModel.query.get(i[0])
-        res.append((i[0], u.fullname, i[1]))
+    for u in all:
+        count_pictures = (len(u.pictures))
+        count_shared_to_me = (len(u.shares))
+        count_share_to_everybody = 0
+        for pic in u.pictures:
+            count_share_to_everybody += len(pic.shares)
+        print(count_pictures, count_shared_to_me, count_share_to_everybody)
+        res.append({"id": u.id, "fullname": u.fullname, 'count_pictures': count_pictures,
+                    "count_shared_to_me": count_shared_to_me, "count_share_to_everybody": count_share_to_everybody
+                    })
     return json.dumps({"statistic": res})
--- src/python/service/Service.py
+++ src/python/service/Service.py
@@ -1,104 +1,117 @@
-from models import UserLoginModel,PictureModel,SharePictureModel,db
+from models import UserLoginModel, PictureModel, SharePictureModel, db
 from sqlalchemy import exc
 from sqlalchemy.sql.expression import func
 
 
 class AuthService():
-    def login(self,username,password):
+    def login(self, username, password):
         username = username.strip()
         password = password.strip()
 
-        u = UserLoginModel.query.filter(UserLoginModel.username==username,
-            UserLoginModel.password==password).first()
+        u = UserLoginModel.query.filter(UserLoginModel.username == username,
+                                        UserLoginModel.password == password).first()
         return u
-    def getByID(self,userlogin_id):
+
+    def getByID(self, userlogin_id):
         return UserLoginModel.query.get(int(userlogin_id))
+
     def getAll(self):
         return UserLoginModel.query.all()
-    def getByUsername(self,username):
-        return  UserLoginModel.query.filter(UserLoginModel.username==username).first()
-    def register(self,username,password,fullname):
-        u = UserLoginModel(username=username,password=password,fullname=fullname)
+
+    def getByUsername(self, username):
+        return UserLoginModel.query.filter(UserLoginModel.username == username).first()
+
+    def register(self, username, password, fullname):
+        u = UserLoginModel(username=username,
+                           password=password, fullname=fullname)
         db.session.add(u)
         db.session.commit()
         db.session.refresh(u)
         return u
-    
+
+
 class PictureService():
-    def is_Permission(self,userlogin_id,picture_id):
-        userlogin_id= int(userlogin_id)
-        picture_id=int(picture_id)
-        pic= PictureModel.query.filter(PictureModel.userlogin_id==userlogin_id,
-                            PictureModel.id==picture_id).first()
+    def is_Permission(self, userlogin_id, picture_id):
+        userlogin_id = int(userlogin_id)
+        picture_id = int(picture_id)
+        pic = PictureModel.query.filter(PictureModel.userlogin_id == userlogin_id,
+                                        PictureModel.id == picture_id).first()
         return not not pic
 
-    def insert(self,userlogin_id,pic):
-        picture = PictureModel(userlogin_id=userlogin_id,pic=pic)
+    def insert(self, userlogin_id, pic):
+        picture = PictureModel(userlogin_id=userlogin_id, pic=pic)
         db.session.add(picture)
         try:
             db.session.flush()
             db.session.refresh(picture)
-            picture.pic= str(picture.userlogin_id)+"/"+str(picture.id)+"_"+picture.pic
+            picture.pic = str(picture.userlogin_id)+"/" + \
+                str(picture.id)+"_"+picture.pic
             db.session.merge(picture)
             db.session.commit()
             return picture
         except exc.SQLAlchemyError:
             db.session.rollback()
-            return None;
-    def getByUserID(self,userlogin_id):
+            return None
+
+    def getByUserID(self, userlogin_id):
         # lay toan bo anh cua user
-        pics= PictureModel.query.filter(PictureModel.userlogin_id==userlogin_id)
+        pics = PictureModel.query.filter(
+            PictureModel.userlogin_id == userlogin_id)
         return pics
-    def searchByPicID(self,userlogin_id,picture_id):
+
+    def searchByPicID(self, userlogin_id, picture_id):
         # lay thong tin cua anh duoc chia se toi user
         u = AuthService().getByID(userlogin_id)
         # tim trong list anh da su hu
         for pic in u.pictures:
-            if(pic.id==picture_id):
+            if(pic.id == picture_id):
                 return pic
         # tim trong list share
         for sh in u.shares:
-            if(sh.picture_id==picture_id):
+            if(sh.picture_id == picture_id):
                 return sh.picture
         # ko tim thay
         return None
-    def getPicByID(self,picture_id):
+
+    def getPicByID(self, picture_id):
         return PictureModel.query.get(int(picture_id))
 
+
 class ShareService():
-    def getByUserID(self,userlogin_id):
+    def getByUserID(self, userlogin_id):
         # lay picture share cho user_id
-        shares = SharePictureModel.query.filter(SharePictureModel.userlogin_id==userlogin_id)
-        return shares
-    
-    
-    def searchByPicID(self,picture_id):
-        shares =SharePictureModel.query.filter(SharePictureModel.picture_id==picture_id)
+        shares = SharePictureModel.query.filter(
+            SharePictureModel.userlogin_id == userlogin_id)
         return shares
 
-    def searchAvailableUser(self,picture_id):
-        pic =PictureModel.query.get(picture_id)
+    def searchByPicID(self, picture_id):
+        shares = SharePictureModel.query.filter(
+            SharePictureModel.picture_id == picture_id)
+        return shares
+
+    def searchAvailableUser(self, picture_id):
+        pic = PictureModel.query.get(picture_id)
         shares = pic.shares
         list = [sh.userlogin_id for sh in shares]
-        list.append(pic.userlogin_id)# append them id chu? picture
-        users= UserLoginModel.query.filter(UserLoginModel.id.notin_(list)).all()
+        list.append(pic.userlogin_id)  # append them id chu? picture
+        users = UserLoginModel.query.filter(
+            UserLoginModel.id.notin_(list)).all()
         return users
-    
-    
-    def insertMore(self,picture_id,list_user):
+
+    def insertMore(self, picture_id, list_user):
         for userlogin_id in list_user:
-            self.insert(picture_id,userlogin_id)
+            self.insert(picture_id, userlogin_id)
         try:
             db.session.commit()
             return True
         except exc.SQLAlchemyError:
             db.session.rollback()
-            return False;
-        
-    def insert(self,picture_id,userlogin_id):
+            return False
+
+    def insert(self, picture_id, userlogin_id):
         share = SharePictureModel(picture_id=picture_id,
-                      userlogin_id=userlogin_id)
-        try:  
+                                  userlogin_id=userlogin_id)
+        try:
             db.session.add(share)
             db.session.flush()
             db.session.refresh(share)
@@ -106,12 +119,12 @@ class ShareService():
             return True
         except exc.SQLAlchemyError:
             db.session.rollback()
-            return False;
+            return False
         return share
-    
-    def remove(self,picture_id,userlogin_id):
-        share = SharePictureModel.query.filter(SharePictureModel.picture_id==picture_id,
-                    SharePictureModel.userlogin_id==userlogin_id).first()
+
+    def remove(self, picture_id, userlogin_id):
+        share = SharePictureModel.query.filter(SharePictureModel.picture_id == picture_id,
+                                               SharePictureModel.userlogin_id == userlogin_id).first()
         if not share:
             return False
         db.session.delete(share)
@@ -120,21 +133,4 @@ class ShareService():
             return True
         except exc.SQLAlchemyError:
             db.session.rollback()
-            return False;
-
- 
-class StatisticService():
-    def getStatisticShared(self):
-        return db.session.query(
-                SharePictureModel.userlogin_id, 
-                func.count('*').label("total_counts_share")
-            ).group_by(
-                SharePictureModel.userlogin_id
-            ).all()
-            
-
-if __name__=='__main__':
-    a= StatisticService().getStatisticShared()
-    print(a)
-
-    
+            return False
